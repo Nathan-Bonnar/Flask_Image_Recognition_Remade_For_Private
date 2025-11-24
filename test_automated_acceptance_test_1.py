3b3c00c5eb8909ce0a6b5e73ba18088572b86d2d
Nathan Bon <nbonnar17@gmail.com>
2025-11-24 17:21:36 -0500

    more changes for automated testing

--- test_automated_acceptance_test_1.py
+++ test_automated_acceptance_test_1.py
@@ -55,7 +55,7 @@ def test_Automated_Acceptance_Test_One():
 
 def setup():
     chrome_options = Options()
-    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--headless=new")
     chrome_options.add_argument("--no-sandbox") 
     chrome_options.add_argument("--disable-dev-shm-usage")
     chrome_options.add_argument("--disable-gpu")
--- test_automated_acceptance_test_2.py
+++ test_automated_acceptance_test_2.py
@@ -61,7 +61,7 @@ def test_Automated_Acceptance_Test_Two():
 
 def setup():
     chrome_options = Options()
-    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--headless=new")
     chrome_options.add_argument("--no-sandbox") 
     chrome_options.add_argument("--disable-dev-shm-usage")
     chrome_options.add_argument("--disable-gpu")
